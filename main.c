fa463ecb0a32de04c6711706a6781b5cae405420
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2017-09-11 19:06:43 -0300

    Atualiza o arquivo main.c

--- main.c
+++ main.c
@@ -9,26 +9,25 @@
 
 int main (void)
 {
-	uint8_t	pb0 = 0,
-                pb1 = 1,
-                pb2 = 2,
-                pb3 = 3,
-                pb4 = 4;
-                
+    uint8_t pb0 = 0,
+            pb1 = 1,
+            pb2 = 2,
+            pb3 = 3,
+            pb4 = 4;
 
-  
-	/* set PORTB for output*/
-	DDRB = BV(pb1);
-        setBit(PORTB, pb4); _delay_ms(2000);
-  
-  	while(1) 
-    {	
-        if (isPushed(PINB, pb4))
+    /* set PORTB for output*/
+    DDRB = BV (pb1);
+    setBit (PORTB, pb4);
+    _delay_ms (2000);
+
+    while (1)
+    {
+        if (isPushed (PINB, pb4))
         {
-            toggleBit(PORTB, pb1); 
-            _delay_ms(2000);
-        }               
+            toggleBit (PORTB, pb1);
+            _delay_ms (2000);
+        }
     }
 
-	return 0;
+    return 0;
 }
