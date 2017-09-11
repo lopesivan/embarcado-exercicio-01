7edfadedba78136b998a7a6b37f265dab7df040a
Ivan Carlos <lopesivan.ufrj@gmail.com>
2017-09-11 18:55:08 -0300

    Update main.c

--- main.c
+++ main.c
@@ -5,30 +5,30 @@
 #define setBit(byte, bit)    (byte |= BV(bit))
 #define clearBit(byte, bit)  (byte &= ~BV(bit))
 #define toggleBit(byte, bit) (byte ^= BV(bit))
+#define isPushed(byte, bit)  !(byte & BV(bit))
 
 int main (void)
 {
-    uint8_t     pb0 = 0,
+	uint8_t		pb0 = 0,
                 pb1 = 1,
                 pb2 = 2,
                 pb3 = 3,
                 pb4 = 4;
+                
 
-    /* set PORTB for output*/
-    DDRB = BV (pb1);
-    setBit (PORTB, pb4);
-    _delay_ms (2000);
-
-    while (1)
-    {
-
-        if ((PINB & BV (pb4)) == 0)
+  
+	/* set PORTB for output*/
+	DDRB = BV(pb1);
+    setBit(PORTB, pb4); _delay_ms(2000);
+  
+  	while(1) 
+    {	
+        if (isPushed(PINB, pb4))
         {
-            toggleBit (PORTB, pb1);
-            _delay_ms (2000);
-        }
+        	toggleBit(PORTB, pb1); 
+            _delay_ms(2000);
+        }               
+	}
 
-    }
-
-    return 0;
+	return 0;
 }
